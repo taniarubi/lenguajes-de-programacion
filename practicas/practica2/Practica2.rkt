d229fac92623ab6dce685c1850ba10e34253c9ab
Tania Rubí <taniarubi@ciencias.unam.mx>
2020-10-14 19:13:28 -0500

    Primeros 5 ejercicios terminados.

---
+++ practicas/practica2/Practica2.rkt
@@ -0,0 +1,87 @@
+#lang plai
+                                                                                
+;; Ejercicio 1.
+;; Una función que reciba un número n, un número r, y devuelva el conjunto de
+;; los primeros r múltiplos de n (empezando por n). 
+;; multiplos: number number -> (listof number)
+(define (multiplos n r)
+  (map (λ (x) (* x n)) (list-tail (range (add1 r)) 1)))
+
+;; Ejercicio 2.
+;; El predicado divisor? que reciba un número n y un número m y devuelva
+;; verdadero si m divide n, falso en otro caso. Si m es cero, lanza un
+;; error.
+;; divisor?: number number -> boolean
+(define (divisor? m n)
+  (if (zero? m)
+      (error 'divisor? "El cero no divide a ningún número")
+      (and (= (modulo n m) 0))))
+
+;; Ejercicio 3.
+;; Función que recibe un número n como parámetro y devuelve el conjunto de
+;; divisores de n.
+;; divisores: number -> (listof number)
+(define (divisores n)
+  (filter (λ (x) (divisor? x n)) (list-tail (range (add1 n)) 1)))
+
+;; Ejercicio 4.
+;; Un predicado que reciba un elemento e y una lista l y decida si e pertenece
+;; a l.
+;; pertenece?: a (listof a) -> boolean
+(define (pertenece? e l)
+  (ormap (λ (x) (equal? x e)) l))
+
+;; Ejercicio 5.
+;; Una función que reciba una lista l con elementos y devuelva una lista de
+;; elementos sin repetir de la lista original
+;; eliminaRepetidos: (listof a) -> (listof a)
+(define (eliminaRepetidos l)
+  (foldr (lambda (x y) (cons x (filter (lambda (z) (not (equal? x z))) y)))
+         '()
+         l))
+
+;; --- Definimos el tipo de dato Punto ---
+;;(define-type Punto
+;;  [Punto (x number?) (y number?)])
+
+;; Ejercicio 6.
+;; Una función que reciba dos puntos p = (x1, y1) y q = (x2, y2); y calcule el
+;; punto medio entre p y q. Si alguno de los dos argumentos no es un punto,
+;; regresa un error.
+;; punto-medio: Punto Punto -> number
+;; (define (punto-medio p q) ...)
+
+;; Ejercicio 7.
+;; Una función que reciba dos puntos p = (x1, y1) y q = (x2, y2); y calcule la
+;; distancia entre p y q. Si alguno de los dos argumentos no es un punto, arroja
+;; error.
+;; distancia: Punto Punto -> number
+;; (define (distancia p q)...)
+
+;; Ejercicio 8.
+;; Define un tipo de dato abstracto para crear figuras geométricas.
+;;(define-type Figura
+;;  [Circulo ]
+;;  [Triangulo]
+;;  [Cuadrado]
+;;  [Rectangulo])
+
+;; Ejercicio 9.
+;; Una función que reciba una figura y calcule el perímetro de la misma.
+;; perimetro: Figura -> number
+;; (define (perimetro fig) ...)
+
+;; Ejercicio 10.
+;; Una función que reciba una figura y calcule el área de la misma.
+;; area: Figura -> number
+;; (define (area fig) ... )
+
+;; ---------- Punto Extra ------------
+;; Ejercicio 11
+;; Una función que reciba una lista l como parámetro y devuelva el elemento con
+;; mayor número de repeticiones en la lista l. Si hay dos o más elementos
+;; repetidos el mismo número de veces, regresa el primero de éstos en aparecer
+;; de izquierda a derecha en la lista original. Si la lista es vacía, lanza un
+;; error.
+;; masRepetido (listof a) -> a
+;; (define (masRepetido l)...)
---
+++ practicas/practica2/test.rkt
@@ -0,0 +1,42 @@
+#lang plai
+(require (file "./Practica2.rkt"))
+
+;; Test para la función multiplos.
+(test (multiplos 73 8) (list 73 146 219 292 365 438 511 584))
+(test (multiplos 20 10) (list 20 40 60 80 100 120 140 160 180 200))
+(test (multiplos 7 9) (list 7 14 21 28 35 42 49 56 63))
+(test (multiplos 12 3) (list 12 24 36))
+
+;; Test para el predicado divisor?
+(test (divisor? 0 3) "divisor?: El cero no divide a ningún número")
+(test (divisor? 5 30) #t)
+(test (divisor? 2 9) #f)
+(test (divisor? 3 25) #f)
+(test (divisor? 2 8) #t)
+
+;; Test para la función divisores.
+(test (divisores 135) (list 1 3 5 9 15 27 45 135))
+(test (divisores 2) (list 1 2))
+(test (divisores 25) (list 1 5 25))
+(test (divisores 100) (list 1 2 4 5 10 20 25 50 100))
+
+;; Test para el predicado pertenece.
+(test (pertenece? 3 (list 4 7 8 9 32 4 1 3)) #t)
+(test (pertenece? 2 (list 1 2 3 4)) #t)
+(test (pertenece? 9 (list 1 2 3 4)) #f)
+(test (pertenece? 'a (list 'z 'v 'k 's)) #f)
+
+;; Test para la función eliminaRepetidos.
+(test (eliminaRepetidos (list 1 2 3 5 6 7 8 4 4 1 6 7 8)) (list 1 2 3 5 6 7 8 4))
+(test (eliminaRepetidos (list 4 3 5 6 4 4 8 9 6 3 5)) (list 4 3 5 6 8 9))
+
+
+;; Test para la función area.
+;; (test (area (Triangulo (Punto 2 2) (Punto 3 6) (Punto 1 9)))
+
+;; ---- Punto extra ----
+;; Test para la función masRepetido.
+;(test (masRepetido (list 1 2 3 3 6 6 7 7)) 3)
+;(test (masRepetido (list 1 2 3 4)) 1)
+;(test (masRepetido (list 1 2 3 4 5 6 3)) 3)
+;(test (masRepetido (list 1 1 2 2 3 4 1)) 1)
