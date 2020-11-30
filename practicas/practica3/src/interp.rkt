2dee5b35ab7161976eab97078dfd465ae3a71803
Tania Rubí <taniarubi@ciencias.unam.mx>
2020-11-30 16:22:04 -0600

    Examen 1: punto extra agregado.

--- practicas/practica3/src/interp.rkt
+++ practicas/practica3/src/interp.rkt
@@ -8,23 +8,39 @@
 ;; subst: WAE symbol WAE -> WAE
 (define (subst expr sub-id value)
   (match expr
-    [(id i) (if (symbol=? i sub-id) value expr)]
+    [(id i) (if (symbol=? i sub-id)
+                value
+                expr)]
     [(num n) expr]
     [(op f sexp) (op f (map (λ (x) (subst x sub-id value)) sexp))]
-    [(with bindings body)
-     (if (and (member sub-id (bindings-id bindings))
-              (λ (x y) (symbol=? x y)))
-         (with (map (λ (b) (subst-value-binding b sub-id value)) bindings)
-               body)
-         (with (map (λ (b) (subst-value-binding b sub-id value)) bindings)
-               (subst body sub-id value)))]
-    [(with* bindings body)
-     (if (and (member sub-id (bindings-id bindings))
-              (λ (x y) (symbol=? x y)))
-         (with* (map (λ (b) (subst-binding b sub-id value)) bindings)
-               body)
-         (with* (map (λ (b) (subst-binding b sub-id value)) bindings)
-               (subst body sub-id value)))]))
+    [(with bindings body) (if (member sub-id (encuentra-ids bindings))
+                              (with (subst-b sub-id value bindings)
+                                    body)
+                              (with (subst-b sub-id value bindings)
+                                    (subst body sub-id value)))]
+    [(with* bindings body) (if (member sub-id (encuentra-ids bindings))
+                              (with* (subst-b sub-id value bindings)
+                                    body)
+                              (with* (subst-b sub-id value bindings)
+                                    (subst body sub-id value)))]))
+
+;; Regresa los id's de una lista de bindings.
+(define (encuentra-ids expr)
+  (map get-id expr))
+
+;; Regresa el id de un binding.
+(define (get-id expr)
+  (match expr
+    [(binding id value) id]))
+
+;; Ejecuta la función subst en una lista de bindings.
+(define (subst-b sub-id value bindings)
+  (map (λ (b) (subst-binding b sub-id value)) bindings))
+
+;; Ejecuta la función subst en un binding.
+(define (subst-binding b sub-id v)
+  (match b
+    [(binding id value) (binding id (subst value sub-id v))]))
                                       
 ;; Toma un árbol de sintáxis abstracta del lenguaje WAE
 ;; y lo interpreta, devolviendo el valor numérico correspondiente
@@ -35,40 +51,27 @@
     [(num n) n]
     [(op f args) (apply f (map (λ (v) (interp v)) args))]
     [(with bindings body)
-     (interp (subst-bindings (interp-bindings bindings) body))]
-    [(with* bindings body) (interp (subst-bindings bindings body))]))
+     (interp (subst-b2 (interp-b bindings) body))]
+    [(with* bindings body) (subst-b2 (interp bindings) (interp body))]))
 
-;; Función para obtener todos los id's.
-(define (bindings-id expr)
-  (map binding-id expr))
+;; Aplica la función subst a una lista de bindings.
+(define (subst-b2 lista body)
+  (if (empty? lista)
+      body
+      (subst-binding2 (car lista) (subst-b2 (cdr lista) body))))
 
-;; Regresa el id de un binding.
-(define (binding-id expr)
-  (match expr
-    [(binding id value) id]))
-
-;; Sustitución de un valor.
-(define (subst-value-binding bind sub-id val)
-  (type-case Binding bind
-    [binding (id value) (binding id (subst value sub-id val))]))
-
-;; Sustitución para un binding.
-(define (subst-binding expr body)
+;; Aplica la función subst en un binding.
+(define (subst-binding2 expr body)
   (match expr
     [(binding id value) (subst body id value)]))
 
-;; Aplica la sustitución a una lista de bindings
-(define (subst-bindings lista body)
+;; Aplica la función interp a una lista de bindings.
+(define (interp-b lista)
   (if (empty? lista)
-      body
-      (subst-binding (car lista) (subst-bindings (cdr lista) body))))
-    
-;; Interpretación de un binding.
-(define (interp-bindings bind-list)
-  (cond
-    [(empty? bind-list) '()]
-    [else
-     (cons
-      (type-case Binding (car bind-list)
-        [binding (id value) (binding id (num (interp value)))])
-      (interp-bindings (cdr bind-list)))]))
+      '()
+      (cons (interp-binding (car lista)) (interp-b (cdr list)))))
+
+;; Aplica la función interp a un binding.
+(define (interp-binding expr)
+  (match expr
+    [(binding id value) (binding id (num (interp value)))]))
--- practicas/practica3/src/parser.rkt
+++ practicas/practica3/src/parser.rkt
@@ -18,27 +18,22 @@
        (case (first sexp)
          ;; Operadores n-narios.
          [(+ - * /)
-          (if (> (length sexp) 2)
-              (op (elige-operador (first sexp)) (map parse (cdr sexp)))
-              (error 'parse "El operador necesita al menos dos operandos."))]
+          (parse-op sexp)]
          ;; Operadores binarios.
          [(modulo expt)
           (if (equal? (length sexp) 3)
-              (op (elige-operador (first sexp)) (map parse (cdr sexp)))
+              (parse-op sexp)
               (error 'parse "El operador es binario."))]
          ;; Operadores unarios.
          [(add1 sub1)
           (if (equal? (length sexp) 2)
-              (op (elige-operador (first sexp)) (map parse (cdr sexp)))
+              (parse-op sexp)
               (error 'parse "El operador es unario."))]
          ;; Asignaciones locales.
-         [(with)
-          (with (map (λ (l) (binding (car l) (parse (cadr l)))) (second sexp))
-                            (parse (third sexp)))]
-         [(with*)
-          (with* (map (λ (l) (binding (car l) (parse (cadr l)))) (second sexp))
-                             (parse (third sexp)))])]
-    [else error 'parse "Expresión inválida."]))
+         [(with) (with (parse-binding sexp) (parse (third sexp)))]
+         [(with*) (with* (parse-binding sexp) (parse (third sexp)))])]
+    [else error 'parse "La expresión no se encuentra dentro de la " +
+                       "gramática WAE."]))
 
 ;; Función auxiliar. Regresa el operador que le corresponde a la expresión.
 ;; elige-operador: symbol -> procedure
@@ -52,3 +47,29 @@
     [(expt) expt]
     [(add1) add1]
     [(sub1) sub1]))
+
+;; Aplica la función parse a una expresión que inicia con un operador.
+;; parse-op: s-expression -> WAE
+(define (parse-op sexp)
+  (op (elige-operador (first sexp)) (map parse (cdr sexp))))
+
+;; Aplica la función parse a la lista de bindings de una expresión with.
+;; parse-with: s-expression -> WAE
+(define (parse-binding sexp)
+  (if (equal? (hayDuplicados (encuentraID (second sexp))) #t)
+      (error 'parse "Hay id's repetidos.")
+      (map (λ (b) (binding (car b) (parse (cadr b))))
+           (second sexp))))
+
+;; Nos dice si hay elementos duplicados en una lista.
+(define (hayDuplicados lista)
+  (cond
+    [(empty? lista) #f]
+    [(member (first lista) (cdr lista)) #t]
+    [else (hayDuplicados (cdr lista))]))
+
+;; Regresa todos los binding-id de una expresión with.
+(define (encuentraID lista)
+  (if (empty? lista)
+      '()
+      (cons (car (first lista)) (encuentraID (cdr lista)))))
